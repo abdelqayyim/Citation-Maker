cc1dfeee0386106591bf3afd0ac2354796aa4ede
abdelqayyim <abdelymaster@gmail.com>
2021-10-27 18:29:06 -0400

    Version 2.1.2
    Darkmode Has been Successfully Implemented with Responsiveness with Multiple devices. The website is ready for usage

--- index.js
+++ index.js
@@ -10,13 +10,27 @@ const citationType = document.getElementById("citation-type")
 const button = document.querySelector(".button");
 const form = document.getElementById("form");
 var cite = document.getElementById("cite");
+var INPUTS = document.querySelectorAll("input");
 
+var switchh = document.getElementById("switchh");
+//For some reason I cannot target the buttons when I do querySelectorAll("button"), all that shows is the cite button
 
-// document.cookie = "count=0";
 
 addToPage();
 
-cite.addEventListener("click", function () {printHello() });
+function showInput(item) {
+    item.classList.toggle("dark-mode");
+}
+
+cite.addEventListener("click", function () { printHello() });
+switchh.addEventListener("click", function () { switchMode() }); 
+
+
+function switchMode() {
+    INPUTS.forEach(showInput);
+    document.body.classList.toggle("dark-mode");
+    cite.classList.toggle("dark-mode");
+}
 
 //   The copy to clipboard only works with textArea tags I believe. 
 function printHello() {    //Now add text and decorations
@@ -31,15 +45,10 @@ function printHello() {    //Now add text and decorations
     datePublished.defaultValue = 'undefined';
     dateAccessed.defaultValue = 'undefined';
     url.defaultValue = 'undefined';
-    // var count = document.cookie.split(/; */)[0];
-    // var currentCitation = parseInt(count.substring(count.indexOf("=") + 1));
 
-    // console.log(typeof currentCitation);
-
-    // console.log((typeof parseInt(currentCitation)));
     addToPage(num);
     
-    var site = `<div class="citations" id="citations-${num}"><div class="citation" id="citation-${num}"><textarea name="textArea" id="textArea-${num}">${lastName.value}, ${firstName.value}. "${title.value}". ${publisher.value}, Published ${datePublished.value}. Accessed on ${dateAccessed.value}. ${citationType.value}</textarea></div><div class="buttons"><button class="delete-button" onclick="deleteCitation(${num})"><i class="far fa-trash-alt icon"></i>Delete</button><button class="copy-button" onclick="copyToClickBoard(${num})"><i class="far fa-copy icon"  id="citation-${num}-button"></i></i>Copy</button></div></div>`;
+    var site = `<div class="citations" id="citations-${num}"><div class="citation" id="citation-${num}"><textarea name="textArea" id="textArea-${num}">${lastName.value}, ${firstName.value}. "${title.value}". ${publisher.value}, Published ${datePublished.value}. Accessed on ${dateAccessed.value}. ${citationType.value}</textarea></div><div class="buttons"><button class="delete-button butto" onclick="deleteCitation(${num})"><i class="far fa-trash-alt icon"></i>Delete</button><button class="copy-button butto" onclick="copyToClickBoard(${num})"><i class="far fa-copy icon"  id="citation-${num}-button"></i></i>Copy</button></div></div>`;
 
     addToCookie(lastName.value, site);
     document.cookie = `count=${num+1}`;
@@ -47,17 +56,16 @@ function printHello() {    //Now add text and decorations
 }
 function addToCookie(author, citation) {
     document.cookie = `${author}=${citation}`;
-    // addToPage(author);
 }
 function addToPage(number) {
     var all = document.cookie.split(/; */);
-    for (var i = 0; i < all.length-1; i++){
-        var citations = document.createElement("div");
-        citations.className = "a"+i;
-        // var ok = all[i];
-        citations.innerHTML = all[i].substring(all[i].indexOf("=") + 1);
-        // console.log(ok[i].substring(ok[i].indexOf("=") + 1));
-        document.body.appendChild(citations);
+    for (var i = 0; i < all.length; i++){
+        if (all[i].split("=")[0] != "count") {
+            var citations = document.createElement("div");
+            citations.className = "a"+i + " "+"citee";
+            citations.innerHTML = all[i].substring(all[i].indexOf("=") + 1);
+            document.body.appendChild(citations);
+        }
     }
 }
 
